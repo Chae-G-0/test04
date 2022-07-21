3eaf11ca95c1224bc811a399dd8b99131df62e2c
chae <corkdud@naver.com>
2022-07-21 17:41:39 +0900

    220721

--- js/test04.js
+++ js/test04.js
@@ -7,5 +7,32 @@ $(function () {
         pauseOnFocus: false,
     })
 
+    $('.room').slick({
+        arrows: false,
+        dots: false,
+        autoplay: true,
+        centerMode: true,
+        centerPadding: '200px',
+        pauseOnHover: false,
+        pauseOnFocus: false,
+    })
 
+    $('.arrow i:nth-child(1)').on('click', function () {
+        $('.mainRoom .room').slick('slickPrev')
+    })
+
+    $('.arrow i:nth-child(2)').on('click', function () {
+        $('.mainRoom .room').slick('slickNext')
+    })
+
+
+
+    $('.tapMap>li>a').on('click', function (e) {
+        e.preventDefault();
+        var idx = $(this).parent().index();
+        $('.location>div').removeClass('on')
+        $('.location>div').eq(idx).addClass('on');
+        $('.tapMap>li').removeClass('on');
+        $(this).parent().addClass('on');
+    });
 })
